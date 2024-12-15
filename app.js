e7b1519fe19adea8d8829c8dc21d384238e06eb3
RaiseDennis <75945435+RaiseDennis@users.noreply.github.com>
2024-12-15 20:46:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
